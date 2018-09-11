5163c6380a38b070a3e323fb56ef17bb1c990105
蒋金其 <woshijiangjinqi@163.com>
2018-09-11 16:07:53 +0800

    test

--- hello.py
+++ hello.py
@@ -5,6 +5,7 @@ app = Flask(__name__)     #创建一个wsgi应用
 def hello_world():
     return 'Hello World!' #输出一个字符串
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)             #启动app的调试模式
 
